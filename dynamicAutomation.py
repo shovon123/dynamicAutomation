341d713d541ed6dd168b5378d5e8c411af53a514
shovon <shovon.xyz@gmail.com>
2024-04-25 23:26:06 +0600

    V2: Refactor logs + added columns to find repetitive behavior

--- dynamicAutomation.py
+++ dynamicAutomation.py
@@ -1,26 +1,10 @@
-"""
-- READ DATA
-- REMOVE DUPLICATE DATA  (TO AVOID ANOMALY WHEN IT RECEIVES SAME LOG TWICE)
-- CONVERT DATA COLUMN TYPE (OBJECT --> DATETIME)
-- SORT BY DATE, TIME (TO AVOID ANOMALY AND CHECK BEHAVIOR SEQUENTIALLY)
-- ADD A DAY COLUMN TO FIGURE OUT EACH DAY'S BEHAVIOR
-- ADD A BEHAVIOR COLUMN MERGING ALL THE OTHER COLUMN
-- A BEHAVIOR DICTIONARY HOLDING CONFIDENCE OF EACH EXISTING BEHAVIOR
-- CHECK EACH ROW OF LOG AND FIGURE OUT IF THE BEHAVIOR IS NEW
-    - IF IT IS NEW, SET INITIAL CONFIDENCE (EX: 50)
-    - IF OLD, INCREASE CONFIDENCE (ALPHA = 0.3, EVENT = 100) THROUGH SMOOTH EXPONENTIAL FORMULA
-
-WHAT THIS IMPLEMENTATION LACKS
-- DECREASING CONFIDENCE IN ABSENCE OF A EXISTING BEHAVIOR (PRE-DEFENSE #3)
-- ROUND UP TIME (PRE-DEFENSE #4)
-- THRESHOLD NOT ADDED (>75) TO PREDICT IF IT IS REPETITIVE. (PRE-DEFENSE #5)
-"""
 import pandas as pd
+pd.set_option('display.max_rows', 10)
 
-def round_time(time_str):   #EX: 9:29 --> 9, 9:30 --> 10
+def round_time(time_str):                       #EX: 9:29 --> 9, 9:30 --> 10
     hour, minute = str(time_str).split(":")
 
-    new_minute = '00' + minute[2:]
+    new_minute = '00' + minute[2:]              #minute[2:0] --> AM/PM
     if int(minute[0]+minute[1]) < 30:
         new_time = f"{hour}:{new_minute}"
         return new_time
@@ -30,35 +14,41 @@ def round_time(time_str):   #EX: 9:29 --> 9, 9:30 --> 10
         new_time = f"{new_hour}:{new_minute}"
         return new_time
 
-i = 0
-def dfprint(df, str):
-    global i; i+=1
-    print(f"\n({i}) ===== {str} =======\n{df.head(4)}")
+def dfprint(str):   print(f"\n======= {str} =======\n{df.head(4)}")
 
-def comment(str):
-    global i; i+=1
-    print(f"\n({i}) ===== {str} =======\n")
+def comment(str):   print(f"\n======= {str} =======")
+
+df = pd.read_csv("behavior.csv")
 
 def main():
-    df = pd.read_csv("behavior.csv")
-    dfprint(df, "Original Data")
+    dfprint(f"({0}) ORIGINAL Data")
 
     df.columns = df.columns.str.replace(' ', '')
-    dfprint(df, "Removed leading and Trailing Space (Error Handling)")
+    dfprint(f"({1}) Removed LEADING and TRAILING SPACE (Error Handling)")
 
     df.drop(['deviceID', 'eventID'], axis=1, inplace=True)
-    dfprint(df, "Removed USELESS columns (IDs doesn't help find behaviors)")
+    dfprint(f"({2}) Removed USELESS COLUMNS (IDs doesn't help find behaviors)")
 
     df['time'] = df['time'].apply(lambda x: round_time(x))
-    dfprint(df, "Rounded the time (repetitive behavior happens in similar time)")
+    dfprint(f"({3}) ROUNDED the TIME (repetitive behavior happens in similar time)")
 
     df.drop_duplicates(inplace=True)
-    dfprint(df, "removed DUPLICATE rows (to avoid the anomaly of receiving same data twice)")
+    dfprint(f"({4}) removed DUPLICATE ROWS (to avoid the anomaly of receiving same data twice)")
 
     oldDateType = df['date'].dtype
     df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y')
     newDateType = df['date'].dtype
-    dfprint(df, f"updated type of 'date' (to use datetime64 functions): {oldDateType} --> {newDateType}")
-    comment("'time' functions are not needed. so it is kept as object")
+    dfprint(f"({5}) UPDATED TYPE of 'DATE' (to use datetime64 functions): {oldDateType} --> {newDateType}")
+    comment(f"({6}) 'TIME' functions are NOT needed. so it is kept as object")
+
+    df.sort_values(by=['date', 'time'], ascending=[True, True], inplace=True)
+    dfprint(f"({7}) Sort by DATE and then by TIME (to observe behavior gradually)")
+
+    df.insert(2, 'day', df['date'].dt.day_name())
+    dfprint(f"({8}) ADDED DAY column (to find weekly pattern)")
+
+    # add another column to hold WHAT THE REPETITIVE PATTER IS named "behavior"
+    df['behavior'] = df['time'] + " " + df['day'] + " " + df['device'] + " " + df['action']
+    dfprint(f"({9}) ADDED BEHAVIOR column (the column to be observed as repetitive)")
 
 main()
